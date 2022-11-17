07abf41f474dd9bc06cc2df1f7f7774ac8d38199
Topias Tiilikainen <topias.tiilikainen@tuni.fi>
2022-11-17 14:03:58 +0200

    all fixed now

--- controllers/guesses.js
+++ controllers/guesses.js
@@ -4,11 +4,11 @@ const Match = require('../models/match')
 const User = require('../models/user')
 
 router.get('/', async (request, response) => {
-  const guesses = await Guess
+    const guesses = await Guess
         .find({})
         .populate('match')
         .populate('user')
-  return response.json(guesses)
+    return response.json(guesses)
 })
 
 router.get('/:id', async (request, response) => {
@@ -22,8 +22,9 @@ router.get('/:id', async (request, response) => {
 
 router.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findOne({ id: body.userId })
-    const match = await Match.findOne({ id: body.matchId })
+    console.log('request.body @guesses.js - Backend :', request.body)
+    const match = await Match.findById(body.matchId)
+    const user = await User.findById(body.userId)
 
     const newGuess = new Guess({
         match: match._id,
@@ -33,27 +34,36 @@ router.post('/', async (request, response) => {
     })
     
     const savedGuess = await newGuess.save()
+    savedGuess.populate('match')
+    savedGuess.populate('user')
     await User.findByIdAndUpdate(body.userId, { guesses: user.guesses.concat(savedGuess._id) }, { new: true })
     return response.status(201).json(savedGuess)
 })
 
 router.delete('/:id', async (request, response) => {
-    const guess = await Guess.findOne({ id: request.params.id })
-    const user = await User.findOne({ id: guess.user._id })
+    const guess = await Guess.findById(request.params.id)
+    const user = await User.findById(guess.user._id)
     if (!user || !guess) {
         return response.status(404).end()
     }
     user.guesses = user.guesses.filter(g => g.toString() !== guess._id.toString())
-    user.points = user.points - guess.points
+    if (guess.points > 0) {
+        user.points = user.points - guess.points
+    }
+    else {
+        user.points = guess.points + user.points
+    }
     await user.save()
     await guess.remove()
     return response.status(204).end()
 })
 
 router.put('/:id', async (request, response) => {
-  const guess = request.body
-  const updatedGuess = await Guess.findByIdAndUpdate(request.params.id, guess, { new: true })
-  return response.json(updatedGuess)
+    const guess = request.body
+    const updatedGuess = await Guess.findByIdAndUpdate(request.params.id, guess, { new: true })
+    await updatedGuess.populate('match')
+    await updatedGuess.populate('user')
+    return response.json(updatedGuess)
 })
 
 module.exports = router
